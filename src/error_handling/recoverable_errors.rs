8c2645d02b127d94d360671d5abc769a53529075
nehjain.2001 <nehjain.2001@gmail.com>
2024-08-31 12:27:43 +0000

    RECOVERABLE ERRORS

--- src/error_handling/recoverable_errors.rs
+++ src/error_handling/recoverable_errors.rs
@@ -1,20 +1,25 @@
-use std::env::current_dir;
-use std::fs::{canonicalize, File};
+use std::fs::{File, OpenOptions};
 use std::io::{Error, ErrorKind, Read, Write};
-use std::path::{Path, PathBuf};
 
 fn using_match() {
-    let current_directory = canonicalize(Path::new("src/assets/Cargo.lock"));
-
     let mut lock_file_string: String = String::new();
-    let lock_file_result: Result<File, Error> = File::open(current_directory);
+    let current_directory: &str = "src/assets/text.txt";
+    let lock_file_result: Result<File, Error> = File::open(&current_directory);
 
     let mut lock_file: File = match lock_file_result {
         Ok(file) => file,
         Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(current_directory) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem opening the file: {e:?}"),
+            ErrorKind::NotFound => match OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(&current_directory)
+            {
+                Ok(mut fc) => match fc.write_all("Hello Lock".as_bytes()) {
+                    Ok(_) => fc,
+                    Err(e) => panic!("Problem writing to the file: {e:?}"),
+                },
+                Err(e) => panic!("Problem creating the file: {e:?}"),
             },
             other_error => {
                 panic!("Problem opening the file: {other_error:?}");
@@ -22,11 +27,21 @@ fn using_match() {
         },
     };
 
-    lock_file.read_to_string(&mut lock_file_string);
+    lock_file.read_to_string(&mut lock_file_string).unwrap();
 
     println!("Lock file: {}", lock_file_string);
 }
 
+fn chaining() -> Result<String, Error> {
+    let mut lock_file_string: String = String::new();
+    let current_directory: &str = "src/assets/text.txt";
+
+    let _ = File::open(&current_directory)?.read_to_string(&mut lock_file_string);
+
+    Ok(lock_file_string)
+}
+
 pub fn recoverable_errors() {
     using_match();
+    let _ = chaining();
 }
